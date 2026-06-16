801fcc0597033407885aacb6c03a0485ac65f17f
chiranjit <mondalchiranjit2007@gmail.com>
2026-06-17 00:05:45 +0530

    List Practise 1

---
+++ List/02_Problem2.py
@@ -0,0 +1,12 @@
+arr = []
+
+nums1 = int(input("Enter a number 1:"))
+arr.append(nums1)
+
+nums2 = int(input("Enter a number 2:"))
+arr.append(nums2)
+
+nums3 = int(input("Enter a number 3:"))
+arr.append(nums3)
+
+print(arr[1])
